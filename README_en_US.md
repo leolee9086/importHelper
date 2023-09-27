30923b654a62536a236448a0b2d04f9f0c89c2a4
leolee9086 <464803712@qq.com>
2023-09-27 21:30:00 +0800

    创建插件

---
+++ README_en_US.md
@@ -0,0 +1,15 @@
+Purpose
+
+To parse and convert wiki links into standard markdown links for easy import.
+How to use
+
+After installation, an icon will be added to the top bar. Clicking on it will bring up a folder selection box. Select the folder you want to parse.
+Precautions
+
+It should support aliases and titles, etc.
+
+After the parsing is completed, a folder ending with the source folder name plus 'prepared' will be added to the same level directory of the original folder.
+
+Each file processing will generate a .log.md file.
+
+To avoid mis-import, the result file will not be automatically imported after parsing is completed.
---
+++ index.js
@@ -0,0 +1,175 @@
+const { Plugin } = require("siyuan");
+const clientApi = require("siyuan");
+const path = globalThis.require('path')
+const fs = globalThis.require('fs')
+const { dialog } = globalThis.require('@electron/remote');
+let glob,MagicString
+class importHelper extends Plugin {
+    onload() {
+        console.log(this)
+        this.创建顶栏按钮()
+        this.初始化环境变量()
+        this.加载事件()
+        this.加载依赖()
+    }
+    初始化环境变量() {
+        this.selfURL = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}/plugins/${this.name}/`;
+        this.dataPath = `/data/storage/petal/${this.name}`
+        this.tempPath = `/temp/ccTemp/${this.name}/`
+        this.publicPath = `/data/public`
+        this.selfPath = `/data/plugins/${this.name}`
+        this.localPath = path.join(window.siyuan.config.system.workspaceDir, 'data', 'plugins', this.name)
+        }
+    加载事件(){
+        this.顶栏按钮.addEventListener(
+            'click', () => {
+                dialog.showOpenDialog({
+                    properties: ['openDirectory']
+                }).then(result => {
+                    if (!result.canceled) {
+                        const folderPath = result.filePaths[0]; // 获取选中的文件夹路径
+                        const normalizedPath = path.normalize(folderPath).replace(/\\/g,'/');
+                        const targetFolderPath = path.join(normalizedPath, '..', path.basename(normalizedPath) + '_prepared');
+                        if (!fs.existsSync(targetFolderPath)) {
+                            fs.mkdirSync(targetFolderPath);
+                        }
+                        let list = glob.sync("**", { stats: true, cwd: normalizedPath });
+                        let logFilePath = path.join(targetFolderPath, `prepare${Date.now()}.log.md`);
+                        let logFile = fs.createWriteStream(logFilePath, { flags: 'a' });
+                        for (let i = 0, len = list.length; i < len; i++) {
+                            let item = list[i];
+                            item.filePath = path.join(normalizedPath, item.path);
+                            let targetFilePath = path.join(targetFolderPath, item.path);
+                            try {
+                                if (item.name.endsWith(".md")) {
+                                    item.markdown = fs.readFileSync(item.filePath, "utf-8");
+                                    if (item.markdown && item.markdown.length) {
+                                        item = 解析文本(item, '', list);
+                                        let targetDir = path.dirname(targetFilePath);
+                                        if (!fs.existsSync(targetDir)) {
+                                            fs.mkdirSync(targetDir, { recursive: true });
+                                        }
+                                        fs.writeFileSync(targetFilePath, item["markdown_prepared"]);
+                                    }
+                                } else {
+                                    let targetDir = path.dirname(targetFilePath);
+                                    if (!fs.existsSync(targetDir)) {
+                                        fs.mkdirSync(targetDir, { recursive: true });
+                                    }
+                                    fs.copyFileSync(item.filePath, targetFilePath);
+                                }
+                                logFile.write(`Processed file: ${item.filePath}\n`);
+                            } catch (err) {
+                                logFile.write(`Error processing file: ${item.filePath}\n`,err,err.stack);
+                                logFile.write(`Error message: ${err.message}\n`,err,err.stack);
+                            }
+                        }
+                        logFile.end();
+                    }
+                }).catch(err => {
+                    console.error(err);
+                });
+            }
+        )
+    }
+    加载依赖(){
+        glob = globalThis.require(path.join(this.localPath,'node_modules','fast-glob'))
+        MagicString = globalThis.require(path.join(this.localPath,'node_modules','magic-string'))
+    }
+    创建顶栏按钮() {
+        this.顶栏按钮 = this.addTopBar(
+            {
+                icon: 'iconDownload',
+                title: '选择文件夹开始处理',
+                position: 'right',
+            }
+        )
+    }
+}
+module.exports = importHelper
+function 读取Markdown文本(item) {
+    let markdown = fs.readFileSync(item.filePath, "utf-8");
+    return markdown;
+}
+function 解析文本(item, flag, list) {
+    let markdown = 读取Markdown文本(item);
+    item["lines"] = 解析文本链接(markdown,flag, list,item);
+    item["markdown_prepared"] = item["lines"].map(item => {return item.markdown}).join('\n');
+    console.log(item)
+    return item;
+}
+function 解析文本链接(markdown,flag, list,item) {
+    let lines = markdown.split(/\r\n\r\n|\n\n|\r\r/);
+    for (let i = 0, len = lines.length; i < len; i++) {
+        lines[i] = 解析一行文本(lines[i], list,item);
+    }
+    return lines;
+}
+function 解析一行文本(line, list, item) {
+    let links = [];
+    let magicline = new MagicString(line);
+
+    if (!line) {
+        return {
+            markdown: magicline.toString(),
+            links: links,
+        };
+    }
+    const regexWikiGlobal = /\[\[([^\]]*)\]\]/g;
+    let wikiMatches = line.match(regexWikiGlobal);
+
+    if (!wikiMatches) {
+        return {
+            markdown: magicline.toString(),
+            links: links,
+        };
+    }
+    wikiMatches.forEach(wikilink => {
+        let {start, end, alias, name, fileName, query, path} = parseWikiLink(wikilink, line, list);
+        links.push({
+            href: path,
+            query: query,
+            hrefName: fileName,
+            alias: alias
+        });
+        magicline.overwrite(start, end, `[${alias}](${path}${query})`);
+    });
+    return {
+        markdown: magicline.toString(),
+        links: links,
+    };
+}
+
+function parseWikiLink(wikilink, line, list) {
+    let start = line.indexOf(wikilink);
+    let end = start + wikilink.length;
+    let alias = wikilink.split("|")[1];
+    let name = wikilink.split("|")[0];
+    if (name) {
+        name = name.substring(2);
+    }
+    if (alias) {
+        alias = alias.substring(0, alias.length - 2);
+    }
+    if (!alias) {
+        name = name.substring(0, name.length - 2);
+        alias = name;
+    }
+    name = name.replace("#", "?title=");
+    name = name.replace("^", "?id=");
+    let fileName = name.split("?")[0];
+    let query = name.replace(fileName, "");
+    if ((fileName.indexOf("\.") < 0)) {
+        fileName = fileName + '.md';
+    }
+    let linktarget = list.find((e) => {
+        return e.path == fileName;
+    });
+    if (!linktarget) {
+        linktarget = list.find((e) => {
+            return e.name == fileName;
+        });
+    }
+    let path = linktarget ? linktarget.path : fileName;
+    return {start, end, alias, name, fileName, query, path};
+}
